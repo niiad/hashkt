0f2bb1098fac999616b9993ecc3c416f56b70555
Nii Adotei <samuelbrownai@gmail.com>
2023-07-21 01:32:14 +0000

    initialized project, added base functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import base.areaOfCircle
+import base.celsiusToFahrenheit
+
+fun main() {
+    println("The area is ${areaOfCircle(5.0)}")
+    println("45 deg celsius is ${celsiusToFahrenheit(45.0)} fahrenheit")
+}
